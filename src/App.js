aa65fd26a6ffd4c6972dd23d33aa0f649474c567
Saurav Datta <sauravdatta06@gmail.com>
2021-08-11 21:19:39 +0600

    new component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import MainComponent from './components/MainComponent.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <MainComponent />
     </div>
   );
 }
